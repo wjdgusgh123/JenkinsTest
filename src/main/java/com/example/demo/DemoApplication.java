18a5aaa16656ab8b0515d4c0a68dd6b06d5a5af2
JHH <gusgh9098@naver.com>
2024-05-27 14:06:27 +0900

    First Jenkins Test

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value="/")
+	public String getHelloWorld(){
+		return "Hello World";
+	}
+
+	@GetMapping(value="/demo")
+	public String GetHelloWorldDemo() {
+		return "Hello World Demo";
+	}
+}
