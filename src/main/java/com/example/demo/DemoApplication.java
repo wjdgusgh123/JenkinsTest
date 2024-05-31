6f9a524cdc32597665b4c579140a46ff43a234ba
JHH <gusgh9098@naver.com>
2024-05-31 15:29:32 +0900

    port 12003으로 변경

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
